6ac4c91db2baaf4cfca6b42c73964216eeb9b82c
luabraga0211 <141754027+luabraga0211@users.noreply.github.com>
2024-11-12 08:53:57 -0300

    tat
    at

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000)
+const users = [
+  {
+    name: 'Luã',
+    email: 'luasergiogomesbraga@gmail.com',
+    idade: 18, 
+    sexo: 'Masculino'
+  }
+]
+
+app.get('/usuarios', (req, res) => {
+  res.json(users)
+})
+
+app.post('/usuarios', (req, res) => {
+  console.log(req.body)
+  users.push(req.body)
+  res.status(201).json(users)
+})
+
+app.listen(3000, () => console.log("Servidor está rodando"))
